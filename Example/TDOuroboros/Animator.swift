27e41cc76c75bca917859114907cb072f3283865
michaeldai1006 <dtc74110@gmail.com>
2018-09-03 16:29:59 -0700

    Create and implement Animator

---
+++ Example/TDOuroboros/Animator.swift
@@ -0,0 +1,53 @@
+//
+//  Animator.swift
+//  TDOuroboros_Example
+//
+//  Created by Michael Open Source on 9/3/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Animator {
+    static func performJumpAnimation(forView view: UIView,
+                                     withDistance distance: CGFloat,
+                                     withDuration duration: Double,
+                                     withDamping damping: CGFloat,
+                                     withInitVelocity initVelocity: CGFloat,
+                                     withScale scale: CGFloat,
+                                     complete: (() -> Void)?) {
+        
+        // Task queue instance
+        let taskQueue = TDTaskQueueManager()
+        
+        // Scale up
+        taskQueue.enqueue {
+            UIView.animate(withDuration: duration,
+                           delay: 0.0,
+                           usingSpringWithDamping: damping,
+                           initialSpringVelocity: initVelocity,
+                           options: .curveLinear,
+                           animations: {
+                            view.transform = CGAffineTransform(scaleX: scale, y: scale)
+            }, completion: { (result) in
+                taskQueue.taskCompleted()
+            })
+        }
+        
+        // Scele down
+        taskQueue.enqueue {
+            UIView.animate(withDuration: duration,
+                           delay: 0.0,
+                           usingSpringWithDamping: damping,
+                           initialSpringVelocity: initVelocity,
+                           options: .curveLinear,
+                           animations: {
+                            view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            }, completion: { (result) in
+                taskQueue.taskCompleted()
+                complete?()
+            })
+        } // Scale down
+    } // performJumpAnimation
+} // Animator
