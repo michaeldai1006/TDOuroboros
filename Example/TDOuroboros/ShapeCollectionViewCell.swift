8434a6b9648f06f2376d4c423397f864f17c60a5
michaeldai1006 <dtc74110@gmail.com>
2018-09-03 16:27:17 -0700

    Create and implement ShapeCollectionViewCell

---
+++ Example/TDOuroboros/ShapeCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ShapeCollectionViewCell.swift
+//  TDOuroboros_Example
+//
+//  Created by Michael Open Source on 9/3/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ShapeCollectionViewCell: UICollectionViewCell {
+    static let id = String(describing: ShapeCollectionViewCell.self)
+    @IBOutlet weak var shapeImageView: UIImageView!
+}
